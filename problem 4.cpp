9beeccb1cd5f9bb4407825509eaf13b8efcf1964
ABC9925 <avinashghadage416@gmail.com>
2024-07-11 23:31:02 +0530

    Add files via upload

---
+++ problem 4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, product;
+
+    // Taking input from the user
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Calculate the product
+    product=num1*num2;
+    cout<<"product of two number:"<<product<<endl;
+
+    return 0;
+}
