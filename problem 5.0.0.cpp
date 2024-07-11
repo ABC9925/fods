db115c3ef147ec0e116739b474ef7a18a7fff7bd
ABC9925 <avinashghadage416@gmail.com>
2024-07-11 23:41:54 +0530

    Add files via upload

---
+++ problem 5.0.0.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, quotient;
+
+    // Taking input from the user
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Calculate the quotient
+    quotient=num1/num2;
+    cout<<"quotient of two number:"<<quotient<<endl;
+
+    return 0;
+}
