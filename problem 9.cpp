480361db126ed303e47f6d35d5d90654ec256307
ABC9925 <avinashghadage416@gmail.com>
2024-07-12 17:46:36 +0530

    Add files via upload

---
+++ problem 9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    
+    cout<<"enter your number:"<<endl;
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<"the number is even"<<endl;
+        
+    }
+    else{
+        cout<<"the number is odd"<<endl;
+    }
+    return 0;
+}
