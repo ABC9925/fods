e297e120733e4c06c0018ec5fe49ce1f4ba6b25c
ABC9925 <avinashghadage416@gmail.com>
2024-07-19 00:19:30 +0530

    Add files via upload

---
+++ problem 15.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    // Ensure the input is a positive integer
+    if (n <= 0) {
+        cout << "Please enter a positive integer." << endl;
+        return 1;
+    }
+
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum +=(i*i);
+    }
+
+    cout << "The sum of square of the first " << n << " natural numbers is: " << sum << endl;
+    return 0;
+}
